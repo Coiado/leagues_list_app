a25a8ca2980d64ec535b72c20c3bf840e50f8b6b
Lucas Mota <lmota@jib-home.com>
2023-10-07 17:42:30 +0200

    Initial Commit

---
+++ league-list-app/league_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  league_list_appApp.swift
+//  league-list-app
+//
+//  Created by Lucas Coiado Mota on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct league_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
